c5209a5c6dee1a0da3524172b7773a00597b4040
Cameron Reikes <cameronreikes@gmail.com>
2018-04-23 18:36:20 -0700

    Fix vector example for namespace

--- examples/vector/main.cpp
+++ examples/vector/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv) {
   int left, right;
   right = 3;
   left = 5;
-  Vector2<int> myVec(left, right);
+  ooyw::Vector2<int> myVec(left, right);
   std::cout << "myVec with input (" << left << ") and (" << right
             << "): " << myVec.toString() << std::endl;
   std::cout << "myVec distance: " << myVec.length() << std::endl;
