4c69d7ce6869b9a1d97e25648d195d47c6905023
Cameron Reikes <cameronreikes@gmail.com>
2018-04-23 19:17:59 -0700

    Add display initialization to display class

--- src/include/ooyw/system.hpp
+++ src/include/ooyw/system.hpp
@@ -1,15 +1,18 @@
 #ifndef HPP_SYSTEM
 #define HPP_SYSTEM
 
+#include "ooyw/display.hpp"
 #include "ooyw/settings.hpp"
 
 namespace ooyw {
 class System {
 public:
   System(Settings &);
+  ~System();
 
 private:
   Settings &conf;
+  Display disp;
 };
 }
 
--- src/system.cpp
+++ src/system.cpp
@@ -1,5 +1,17 @@
+#include "ooyw/display.hpp"
+#include "ooyw/error.hpp"
 #include "ooyw/system.hpp"
 
 namespace ooyw {
-System::System(Settings &inSettings) : conf(inSettings){};
+System::System(Settings &inSettings) : conf(inSettings) {
+  if (!al_init()) {
+    throw InitError("al_init");
+  }
+  try {
+    disp = Display(inSettings.displaySize, inSettings.clearColor);
+  } catch (InitError &err) {
+    throw err;
+  }
+};
+System::~System() { al_uninstall_system(); }
 }
