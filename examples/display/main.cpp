d423f2cbd8bc79a4bdf4c857dfd52727a265d4e6
Cameron Reikes <cameronreikes@gmail.com>
2018-04-23 19:18:06 -0700

    Add display example

---
+++ examples/display/main.cpp
@@ -0,0 +1,19 @@
+#include <allegro5/allegro.h>
+#include <iostream>
+#include <ooyw/error.hpp>
+#include <ooyw/system.hpp>
+
+int main(int argc, char **argv) {
+  ooyw::Settings set = ooyw::Settings();
+  set.fps = 60;
+  set.displaySize = ooyw::Vector2<int>(500, 500);
+  set.clearColor = ooyw::SafeColor(0, 0, 0);
+  try {
+    ooyw::System sys = ooyw::System(set);
+  } catch (ooyw::InitError &err) {
+    std::cerr << err.what() << std::endl;
+    return -1;
+  }
+  al_rest(5);
+  return 0;
+}
